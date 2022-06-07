943e7b028c2cc1cba810cd33a6fae2de850d175b
Legno0 <meogaphieu12@gmail.com>
2022-06-08 00:15:21 +0700

    update

--- src/router/Routing.js
+++ src/router/Routing.js
@@ -1,19 +1,29 @@
 import { Routes, Route } from "react-router-dom";
-import Main from "./../pages/Main";
-import Login from "./../pages/Login";
+
+import FormLogin from "./../pages/Login";
 import Register from "./../pages/Register";
-import Product from "./../components/Product";
-import ProductAPI from "./../components/ProductAPI";
+import HomePage from "./../pages/HomePage";
+import Header from "./../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Product from "./../components/Product/Product";
+import Detail from "../components/detail/DetailProduct";
+import Cart from "../components/Cart/Cart";
+import Checkout from "../components/Checkout/Checkout";
 const Routing = () => {
   return (
     <div>
       <Routes>
-        {/* <Route path="/" element={<Main />} /> */}
-        <Route path="/ProductAPI" element={<ProductAPI />} />
-        <Route path='/Product' element={<Product />} />
-        <Route path="/" element={<Login />} />
-        <Route path='/Register' element={<Register />} />
-        
+        <Route path="/Product" element={<Product />} />
+        <Route path="/HomePage" element={<HomePage />} />
+        <Route path="/Footer" element={<Footer />} />
+        <Route path="/Header" element={<Header />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/:slug/:product_id" element={<Detail />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route path="/" element={<FormLogin />} />
+        <Route path="/HomePage" element={<HomePage />} />
+        <Route path="/Cart" element={<Cart />} />
       </Routes>
     </div>
   );
